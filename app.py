983a0b1107ec31353674ed82658922ec5df56bae
ShiwenNi <ShiwenNi@users.noreply.huggingface.co>
2023-03-21 06:31:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(fn=greet, inputs1="text", inputs2="text", outputs="text")
 iface.launch()
